ffd85a3c14e477885c7c2740738ac5f3b13a064a
fisheeesh <swanphyo444@gmail.com>
2025-02-10 23:25:55 +0700

    shortly - hamburger and mobile menu

--- shortly-website/js/script.js
+++ shortly-website/js/script.js
@@ -32,3 +32,14 @@ function formSubmit(e) {
 
 
 linkForm.addEventListener('submit', formSubmit)
+
+const btn = document.getElementById('menu-btn')
+
+document.getElementById('menu-btn').addEventListener('click', navToggle)
+
+
+function navToggle() {
+    btn.classList.toggle('open')
+    document.getElementById('menu').classList.toggle('flex')
+    document.getElementById('menu').classList.toggle('hidden')
+}
